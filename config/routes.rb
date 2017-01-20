1cb3f7a58a5dc7e8cc501f79f0793858cd077957
Navneet Singh <navneet.singh.it@gmail.com>
2017-01-20 15:03:55 +1100

    Devise User authentication added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+	authenticated :user do
+    root :to => "dashboard#index"
+  end
+	root to: 'landing_page#show'
+  devise_for :users, controllers: {
+		sessions: 'users/sessions',
+		passwords: 'users/passwords',
+		registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
